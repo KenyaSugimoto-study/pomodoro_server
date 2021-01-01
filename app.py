95be0782f36e7a612dcd32347a379bb733f013a5
KenyaSugimoto <45161125+KenyaSugimoto@users.noreply.github.com>
2021-01-01 10:28:42 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
-from flask import Flask
+from flask import Flask, request
 import pprint
+from flask_cors import CORS
 
 import db_sample as db
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route("/")
 def hello_world():
@@ -14,12 +16,14 @@ def hello_world():
     return result[0]
 
 @app.route("/user_info", methods=["POST"])
-def fetch_user_info(user_id):
-    connection = db.connect_db()
-    result = db.fetch_user_info(connection, user_id)
-    pprint.pprint(result)
-    connection.commit()
-    return result[0]
+def fetch_user_info():
+    # connection = db.connect_db()
+    # result = db.fetch_user_info(connection, user_id)
+    # pprint.pprint(result)
+    # connection.commit()
+    res = request.get_data()
+    pprint.pprint(res)
+    return res
 
 if __name__ == "__main__":
     app.run(debug=True)
